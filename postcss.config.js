2e440ff15fd417b1d4f64f2936598ca3c426fb60
Seu Nome <seu.email@exemplo.com>
2025-05-08 01:45:25 -0300

    Fix: Manually corrected postcss.config.js content

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
->>>>>>> d8653eea6d74965fce481e3ab2daed1e7017c225
 }; 
